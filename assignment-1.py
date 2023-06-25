aea517348a33d0729324b0ffe40dd812a2fa1426
Rom-Patel <137058661+Rom-Patel@users.noreply.github.com>
2023-06-25 13:06:56 +0530

    Add files via upload

---
+++ assignment-1.py
@@ -0,0 +1,7 @@
+
+n1 = int(input("Enter 1st number: "))
+n2 = int(input("Enter 2nd number: "))
+n3 = int(input("Enter 3rd number: "))
+sum = n1+n2+n3
+avg = sum/3
+print("The average of n1,n2 and n3 is - ",avg)
